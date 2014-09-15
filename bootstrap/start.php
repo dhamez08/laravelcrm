67321845f9e1425d8e0a15adfb22ba1ec769a01c
mamalias23 <mamalias23@gmail.com>
2014-09-15 20:14:50 +0800

    added elias config

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,7 +27,8 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 
 	'local' => array('homestead','allanpc-System-Product-Name','allan-R439'),
-	'richard_local' => array('precise32')
+	'richard_local' => array('precise32'),
+	'elias_local' => array('mamalias23-PC')
 
 ));
 
