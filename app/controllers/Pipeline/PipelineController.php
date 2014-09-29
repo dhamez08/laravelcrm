d53b2e6a1ee506214892acd7b321c36e359a9708
mamalias23 <mamalias23@gmail.com>
2014-09-29 20:11:34 +0800

    pipeline update

--- app/controllers/Pipeline/PipelineController.php
+++ app/controllers/Pipeline/PipelineController.php
@@ -26,9 +26,10 @@ class PipelineController extends \BaseController {
 	 * */
 	public function __construct(){
 		parent::__construct();		
-		$this->data_view = parent::setupThemes();		
-		$this->data_view['master_view'] 	= $this->data_view['view_path'] . '.dashboard.index';
+		$this->data_view = parent::setupThemes();
 		$this->destination_path = "/public/document/library/own/";
+		$this->data_view['pipeline_index'] 	= $this->data_view['view_path'] . '.pipeline.index';
+		$this->data_view['master_view'] 	= $this->data_view['view_path'] . '.dashboard.index';
 	}
 
 	/**
@@ -61,59 +62,20 @@ class PipelineController extends \BaseController {
 	 * */
 	public function getIndex(){
 		$data 						= $this->data_view;
-		$data['pageTitle'] 			= 'Document Libraries';
-		$data['portlet_title']		= 'Document Libraries';
-		$data['path']				= $this->destination_path;
-		$data['icons']				= \Config::get('crm.document_file_type_class');
-		$data['documents']			= \DocumentLibrary\DocumentLibraryEntity::get_instance()->documents();
+		$data['pageTitle'] 			= 'Sales Pipeline';
+		$data['portlet_title']		= 'Chart / Sales View';
 		$data 						= array_merge($data,$this->getSetupThemes());
 
-		return \View::make( $data['view_path'] . '.document-libraries.index', $data );
+		return \View::make( $data['view_path'] . '.pipeline.partials.chart-stats-view', $data );
 	}
 
-	public function postUpload() {
-		$rules = array(
-			'name' => 'required',
-			'doc'  => 'required|max:10000|mimes:pdf,doc,docx,xls,xlsx,ppt,pptx,zip,jpg,jpeg,rar,bmp,gif,tif,txt,xml,csv,png'
-		);
+	public function getListView() {
+		$data 						= $this->data_view;
+		$data['pageTitle'] 			= 'Sales Pipeline';
+		$data['portlet_title']		= 'List View';
+		$data 						= array_merge($data,$this->getSetupThemes());
 
-		$messages = array(
-			'name.required' => 'Name is required.',
-			'doc.required'=>'Please select a Document',
-			'doc.mimes'=>'The supported Document file types are : pdf, doc, docx, xls, xlsx, ppt, pptx, zip, jpg, jpeg, rar, bmp, gif, tif, txt, xml, csv and png',
-			'doc.max'=>'Document maximum file size is 10mb',
-		);
-
-		$validator = \Validator::make(\Input::all(), $rules, $messages);
-		if ( $validator->passes() ) {
-			if(\DocumentLibrary\DocumentLibraryEntity::get_instance()->upload()) {
-				\Session::flash('message', 'Document was successfully uploaded');
-				return \Redirect::back();
-			} else {
-				return \Redirect::back()->withErrors(['There was a problem uploading your document, please try again']);
-			}
-		} else {
-			\Input::flash();
-			return \Redirect::back()
-			->withErrors($validator)
-			->withInput();
-		}
-	}
-
-	public function getDelete($id) {
-		$document = \DocumentLibrary\DocumentLibraryEntity::get_instance()->find($id);
-		if($document) {
-			$img = $document->filename;
-			if(\File::delete(public_path().'/document/library/own/'.$img)) {
-				$document->delete();
-				\Session::flash('message', 'Document was successfully deleted');
-				return \Redirect::back();
-			} else {
-				return \Redirect::back()->withErrors(['There was a problem deleting your document, please try again']);
-			}
-		} else {
-			return \Redirect::back()->withErrors(['There was a problem deleting your document, please try again']);
-		}
+		return \View::make( $data['view_path'] . '.pipeline.partials.list-view', $data );
 	}
 
 }
