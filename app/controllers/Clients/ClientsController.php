470412e591e92c8f3d927252b452ba1b2aa2ead4
mamalias23 <mamalias23@gmail.com>
2014-10-15 16:16:57 +0800

    ...

--- app/controllers/Clients/ClientsController.php
+++ app/controllers/Clients/ClientsController.php
@@ -1444,4 +1444,40 @@ class ClientsController extends \BaseController {
 		$data = array('clientid'=>$clientId,'redirect'=>url('clients'));
 		return \Task\TaskController::get_instance()->getAjaxModalCreateTask($data);
 	}
+
+	private function _getClientData($clientId) {
+		$data 						= $this->data_view;
+		$data['pageTitle'] 			= 'Client';
+		$data['contentClass'] 		= '';
+		$data['portlet_body_class']	= 'form';
+		$data['portlet_title']		= 'Client';
+		$data['fa_icons']			= 'user';
+		$group_id					= \User\UserEntity::get_instance()->getUserToGroup()->first()->group_id;
+		$data['customer']			= \Clients\Clients::find($clientId);
+		$data['currentClient']		= \Clients\ClientEntity::get_instance()->bindCustomer($data['customer']);
+		$data['telephone']			= $data['customer']->telephone();
+		$data['email']				= $data['customer']->emails();
+		$data['url']				= $data['customer']->url();
+		$data['profileImg']			= $data['customer']->profileImage();
+		$data['belongToPartner']	= \Clients\ClientEntity::get_instance()->getPartnerBelong($data['customer']);
+		$data['associate']			= \Clients\ClientEntity::get_instance()->setAssociateCustomer($clientId);
+		$data['partner']			= \Clients\ClientEntity::get_instance()->getCustomerPartner();
+		$data['center_column_view']	= 'dashboard';
+		$data['tasks']				= \CustomerTasks\CustomerTasksEntity::get_instance()->getCustomerTasks($clientId)
+		->status(1)->with('label');
+
+		return $data;
+	} 
+
+	public function getCustom($clientId) {
+		$group_id					= \User\UserEntity::get_instance()->getUserToGroup()->first()->group_id;
+		$dashboard_data 			= \Dashboard\DashboardController::get_instance()->getSetupThemes();
+		array_set($dashboard_data,'html_body_class','page-header-fixed page-quick-sidebar-over-content page-container-bg-solid page-sidebar-closed');
+
+		$data = $this->_getClientData($clientId);
+
+		$data 						= array_merge($data,$dashboard_data);
+
+		return \View::make( $data['view_path'] . '.clients.custom', $data );
+	}
 }
