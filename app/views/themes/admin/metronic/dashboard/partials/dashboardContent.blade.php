1a4634d8ddb245004e5cc8edc1f338b92b804637
apysais <allan.paul.casilum@gmail.com>
2014-10-16 16:16:08 +0800

    task widget

--- app/views/themes/admin/metronic/dashboard/partials/dashboardContent.blade.php
+++ app/views/themes/admin/metronic/dashboard/partials/dashboardContent.blade.php
@@ -11,7 +11,7 @@
 <div class="col-lg-6 col-md-6 col-sm-12 col-sx-12">
 	<div class="row">
 		<div class="col-md-12 col-sm-12">
-			{{--\Task\TaskController::get_instance()->getWidgetDisplay()--}}
+
 		</div>
 	</div>
 </div>
