178c293822d7f6afef6a567747f239d8685d05a6
mamalias23 <mamalias23@gmail.com>
2014-10-16 16:23:08 +0800

    ...

--- app/models/CustomFormData/CustomFormData.php
+++ app/models/CustomFormData/CustomFormData.php
@@ -2,4 +2,6 @@
 namespace CustomFormData;
 class CustomFormData extends \Eloquent{
 	protected $table = 'users_custom_forms_data';
+
+	protected $fillable = array('form_id', 'customer_id', 'field_name', 'value');
 }
