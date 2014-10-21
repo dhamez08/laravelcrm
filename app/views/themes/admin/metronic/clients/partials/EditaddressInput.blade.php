c95cf92b3ad4bdb7353a293b0c094fc68261d59e
apysais <allan.paul.casilum@gmail.com>
2014-10-21 15:53:50 +0800

    edit form client

--- app/views/themes/admin/metronic/clients/partials/EditaddressInput.blade.php
+++ app/views/themes/admin/metronic/clients/partials/EditaddressInput.blade.php
@@ -92,6 +92,6 @@
 	}}
 	</div>
 </div>
-@if( isset($customer) )
-{{Form::hidden('address_id', isset($customer->address()->first()->id) ? $customer->address()->first()->id:null)}}
+@if( isset($customer && isset($customer->address()->first()->id)) )
+{{Form::hidden('address_id', $customer->address()->first()->id}}
 @endif
