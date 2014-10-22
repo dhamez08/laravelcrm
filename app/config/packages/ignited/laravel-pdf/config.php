c46d24ecad22fe6b46667914772aa6d910701956
mamalias23 <mamalias23@gmail.com>
2014-10-22 13:26:25 +0800

    ..

--- app/config/packages/ignited/laravel-pdf/config.php
+++ app/config/packages/ignited/laravel-pdf/config.php
@@ -5,10 +5,10 @@ return array(
 	#'bin' => base_path() . '/vendor/h4cc/wkhtmltopdf-i386/bin/wkhtmltopdf-i386',
 	
 	# Uncomment for 64-bit systems
-	#'bin' => base_path() . '/vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64',
+	'bin' => base_path() . '/vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64',
 
 	# Uncomment for 32-bit windows
-	'bin' => base_path() . '/wkhtmltopdf-windows/32/bin/wkhtmltopdf.exe',
+	#'bin' => base_path() . '/wkhtmltopdf-windows/32/bin/wkhtmltopdf.exe',
 
 	# Uncomment for 64-bit windows
 	#'bin' => base_path() . '/wkhtmltopdf-windows/64/bin/wkhtmltopdf.exe',
