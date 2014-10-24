651cb47e82c5ecac46292164d3d5acb03cad2ddb
apysais <allan.paul.casilum@gmail.com>
2014-10-24 20:42:40 +0800

    custom forms box in files page

--- app/views/themes/admin/metronic/files/partials/customforms.blade.php
+++ app/views/themes/admin/metronic/files/partials/customforms.blade.php
@@ -7,17 +7,48 @@
 		</div>
 		<ul class="nav nav-tabs">
 			<li  class="active">
-				<a href=".customformsfile" data-toggle="tab">
+				<a href="#" data-toggle="tab">
 				Files </a>
 			</li>
 		</ul>
 	</div>
 	<div class="portlet-body tabbable-line">
-		<div class="tab-content">
-			<div class="tab-pane active customformsfile" >
-				<h4>List Files</h4>
-			</div>
-		</div>
+		<h4>List Files</h4>
+		@foreach($customerFiles->get()	as $files)
+			@if($files->type == $id)
+				<p>
+					<a href="{{asset('public/documents/' . $files->filename)}}" target="_blank">{{$files->filename}}</a>
+					-
+					{{link_to(
+						'#',
+						$files->name,
+						array(
+							'class'=>'clientFiles editable editable-click',
+							'data-pk'=>$files->id,
+							'data-name'=>'name',
+							'data-type'=>'text',
+							'data-url'=>url('file/ajax-update-name'),
+							'data-title'=>'Update Category',
+						)
+						);
+					}}
+					-
+					<a 	class="btn red btn-sm deleteFile"
+						href="{{
+							action(
+								'File\ClientFileController@getDeleteFile',
+								array(
+									'id'=>$files->id,
+									'customerid'=>$files->customer_id
+								)
+							)
+						}}"
+					>
+						<i class="fa fa-trash-o fa-5x"></i>
+					</a>
+				</p>
+			@endif
+		@endforeach
 	</div>
 </div>
 
