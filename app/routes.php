2063ac3e52a6a6d1108503812181431183f48f24
apysais <allan.paul.casilum@gmail.com>
2014-11-07 21:24:39 +0800

    sms

--- app/routes.php
+++ app/routes.php
@@ -10,10 +10,6 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
-if( \Input::has('datetime') ){
-	\Log::info('textlocal handling receipt ' . \Input::all());
-	\Log::info('textlocal handling receipt ' . print_r(\Input::all()));
-}
 Route::get( '/' , 'HomeController@getIndex' );
 Route::get( 'login' , 'AuthController@getIndex' );
 Route::post( 'login' , 'AuthController@postAuth' );
