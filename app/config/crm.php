3ff8fda7ce6d130fa1e5fa61e2e60c9c9b7e9ed8
apysais <allan.paul.casilum@gmail.com>
2014-11-10 16:50:20 +0800

    sms test false

--- app/config/crm.php
+++ app/config/crm.php
@@ -253,7 +253,7 @@ return array(
 		'api' => array(
 			'hashcode'=>'ea940442105f12c00f8c1172ee78f8352ebdc27d',
 			'login'=>'steve.warden1@btopenworld.com',
-			'test' => true,
+			'test' => false,
 		),
 	),
 	'currency'=>array(
