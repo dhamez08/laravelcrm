ea53113af77e61310ca23b4dd686de4b745bb92d
Elias Jr Mamalias <mamalias23@gmail.com>
2014-11-11 16:08:20 +0800

    asdasd

--- app/controllers/Settings/EmailController.php
+++ app/controllers/Settings/EmailController.php
@@ -2,220 +2,234 @@
 namespace Settings;
 /**
  * Email settings controller
- * 
+ *
  * */
-
 class EmailController extends \BaseController {
 
-	/**
-	 * Instance of this class.
-	 *
-	 * @var      object
-	 */
-	protected static $instance = null;
-	//protected $userEntity;
+    /**
+     * Instance of this class.
+     *
+     * @var      object
+     */
+    protected static $instance = null;
+    //protected $userEntity;
+    /**
+     * hold the view essentials like
+     * - title
+     * - view path
+     * @return array | associative
+     * */
+    protected $data_view;
 
-	/**
-	 * hold the view essentials like
-	 * - title
-	 * - view path
-	 * @return array | associative
-	 * */
-	protected $data_view;
+    /**
+     * Logged in user object
+     *
+     */
+    protected $user;
 
-	/**
-	 * Logged in user object
-	 *
-	 */
-	protected $user;
+    /**
+     * auto setup initialize object
+     * */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->data_view = parent::setupThemes();
+        $this->data_view['settings_index'] = $this->data_view['view_path'] . '.settings.index';
+        //$this->data_view['master_view'] 	= $this->data_view['view_path'] . '.dashboard.index';
+        $this->userEntity = new \User\UserEntity;
+    }
 
-	/**
-	 * auto setup initialize object
-	 * */
-	public function __construct(){
-		parent::__construct();		
-		$this->data_view = parent::setupThemes();
-		$this->data_view['settings_index'] 	= $this->data_view['view_path'] . '.settings.index';
-		//$this->data_view['master_view'] 	= $this->data_view['view_path'] . '.dashboard.index';
-		$this->userEntity = new \User\UserEntity;
-	}
+    /**
+     * Return an instance of this class.
+     *
+     *
+     * @return    object    A single instance of this class.
+     */
+    public static function get_instance()
+    {
+        // If the single instance hasn't been set, set it now.
+        if(null == self::$instance) {
+            self::$instance = new self;
+        }
+        return self::$instance;
+    }
 
-	/**
-	 * Return an instance of this class.
-	 *
-	 *
-	 * @return    object    A single instance of this class.
-	 */
-	public static function get_instance() {
+    /**
+     * get themes
+     * @return    array
+     * */
+    public function getSetupThemes()
+    {
+        return \Dashboard\DashboardController::get_instance()->getSetupThemes();
+    }
 
-		// If the single instance hasn't been set, set it now.
-		if ( null == self::$instance ) {
-			self::$instance = new self;
-		}
+    /**
+     * Index of settings
+     * @return View
+     * */
+    public function getIndex()
+    {
+        $data = $this->data_view;
+        $data['portlet_title'] = 'Email Settings';
+        $data['contentClass'] = '';
+        /* Templates and Signatures */
+        $data['email_templates'] = \User\User::find(\Auth::id())->emailTemplate;
+        $data['email_signatures'] = \User\User::find(\Auth::id())->emailSignature;
+        /* Initialize view data for modals */
+        $dataAddTemplateModal = array();
+        $dataAddSignatureModal = array();
+        $data = array_merge($data, \Dashboard\DashboardController::get_instance()->getSetupThemes());
+        return \View::make($data['view_path'] . '.settings.email.email', $data)
+            ->nest('add_template_modal', $data['view_path'] . '.settings.email.partials.modals.add_template', $dataAddTemplateModal)
+            ->nest('add_signature_modal', $data['view_path'] . '.settings.email.partials.modals.add_signature', $dataAddSignatureModal);
+    }
 
-		return self::$instance;
-	}
+    /**
+     * @return mixed
+     */
+    public function postSaveTemplate()
+    {
+        $emailTemplate = new \EmailTemplate\EmailTemplate;
+        $emailTemplate->belongs_to = \Auth::id();
+        $emailTemplate->name = \Input::get('template_name', '');
+        $emailTemplate->subject = \Input::get('template_subject', '');
+        $emailTemplate->body = \Input::get('template_body', '');
+        $emailTemplate->save();
+        if($emailTemplate) {
+            \Session::flash('message', 'Successfully Added Template');
+        } else {
+            \Session::flash('message', 'Something went wrong');
+        }
+        return \Redirect::to('settings/email');
+    }
 
-	/**
-	 * get themes
-	 * @return	array
-	 * */
-	public function getSetupThemes(){
-		return \Dashboard\DashboardController::get_instance()->getSetupThemes();
-	}
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function getUpdateTemplate($id)
+    {
+        $emailTemplate = \EmailTemplate\EmailTemplate::find($id);
+        $data = $this->data_view;
+        $data['portlet_title'] = 'Update Template';
+        $data['id'] = $emailTemplate->id;
+        $data['name'] = $emailTemplate->name;
+        $data['subject'] = $emailTemplate->subject;
+        $data['body'] = $emailTemplate->body;
+        $data = array_merge($data, \Dashboard\DashboardController::get_instance()->getSetupThemes());
+        return \View::make($data['view_path'] . '.settings.email.update_template', $data);
+    }
 
-	/**
-	 * Index of settings
-	 * @return View
-	 * */
-	public function getIndex() {
-		$data 					= $this->data_view;
-		$data['portlet_title'] 	= 'Email Settings';
-		$data['contentClass'] 	= '';
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function postUpdateTemplate($id)
+    {
+        $emailTemplate = \EmailTemplate\EmailTemplate::find($id);
+        $emailTemplate->name = \Input::get('template_name', '');
+        $emailTemplate->subject = \Input::get('template_subject', '');
+        $emailTemplate->body = \Input::get('template_body', '');
+        $emailTemplate->save();
+        if($emailTemplate) {
+            \Session::flash('message', 'Successfully Updated Template');
+        } else {
+            \Session::flash('message', 'Something went wrong');
+        }
+        return \Redirect::to('settings/email');
+    }
 
-		/* Templates and Signatures */
-		$data['email_templates']  = \User\User::find(\Auth::id())->emailTemplate;
-		$data['email_signatures'] = \User\User::find(\Auth::id())->emailSignature;
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function getRemoveTemplate($id)
+    {
+        $emailTemplate = \EmailTemplate\EmailTemplate::find($id);
+        if($emailTemplate->belongs_to == \Auth::id()) {
+            $emailTemplate->delete();
+            \Session::flash('message', 'Successfully Removed Template');
+        } else {
+            \Session::flash('message', 'Template does not belong to the logged in user.');
+        }
+        return \Redirect::to('settings/email');
+    }
 
-		/* Initialize view data for modals */
-		$dataAddTemplateModal = array();
-		
-		$dataAddSignatureModal = array();
+    /**
+     * @return mixed
+     */
+    public function postSaveSignature()
+    {
+        $emailSignature = new \EmailSignature\EmailSignature;
+        $emailSignature->belongs_to = \Auth::id();
+        $emailSignature->name = \Input::get('signature_name', '');
+        $emailSignature->body = \Input::get('signature_body', '');
+        $emailSignature->save();
+        if($emailSignature) {
+            \Session::flash('message', 'Successfully Added Signature');
+        } else {
+            \Session::flash('message', 'Something went wrong');
+        }
+        return \Redirect::to('settings/email');
+    }
 
-		$data = array_merge($data,\Dashboard\DashboardController::get_instance()->getSetupThemes());
-		return \View::make( $data['view_path'] . '.settings.email.email', $data )
-			->nest('add_template_modal'  , $data['view_path'] . '.settings.email.partials.modals.add_template', $dataAddTemplateModal)
-			->nest('add_signature_modal' , $data['view_path'] . '.settings.email.partials.modals.add_signature', $dataAddSignatureModal);
-	}
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function getUpdateSignature($id)
+    {
+        $emailSignature = \EmailSignature\EmailSignature::find($id);
+        $data = $this->data_view;
+        $data['portlet_title'] = 'Update Template';
+        $data['id'] = $emailSignature->id;
+        $data['name'] = $emailSignature->name;
+        $data['body'] = $emailSignature->body;
+        $data = array_merge($data, \Dashboard\DashboardController::get_instance()->getSetupThemes());
+        return \View::make($data['view_path'] . '.settings.email.update_signature', $data);
+    }
 
-	public function postSaveTemplate()
-	{
-		$emailTemplate = new \EmailTemplate\EmailTemplate;
-		$emailTemplate->belongs_to 	= \Auth::id();
-		$emailTemplate->name 		= \Input::get('template_name', '');
-		$emailTemplate->subject 	= \Input::get('template_subject', '');
-		$emailTemplate->body 		= \Input::get('template_body', '');
-		$emailTemplate->save();
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function postUpdateSignature($id)
+    {
+        $emailSignature = \EmailSignature\EmailSignature::find($id);
+        $emailSignature->name = \Input::get('signature_name', '');
+        $emailSignature->body = \Input::get('signature_body', '');
+        $emailSignature->save();
+        if($emailSignature) {
+            \Session::flash('message', 'Successfully Updated Signature');
+        } else {
+            \Session::flash('message', 'Something went wrong');
+        }
+        return \Redirect::to('settings/email');
+    }
 
-		if($emailTemplate) {
-			\Session::flash('message', 'Successfully Added Template');
-		} else {
-			\Session::flash('message', 'Something went wrong');
-		}
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function getRemoveSignature($id)
+    {
+        $emailSignature = \EmailSignature\EmailSignature::find($id);
+        if($emailSignature->belongs_to == \Auth::id()) {
+            $emailSignature->delete();
+            \Session::flash('message', 'Successfully Removed Signature');
+        } else {
+            \Session::flash('message', 'Signature does not belong to the logged in user.');
+        }
+        return \Redirect::to('settings/email');
+    }
 
-		return \Redirect::to('settings/email');
-	}
-
-	public function getUpdateTemplate($id)
-	{
-		$emailTemplate 		= \EmailTemplate\EmailTemplate::find($id);
-		$data 					= $this->data_view;
-		$data['portlet_title'] 	= 'Update Template';
-		$data['id']			= $emailTemplate->id;
-		$data['name'] 		= $emailTemplate->name;
-		$data['subject']	= $emailTemplate->subject;
-		$data['body']		= $emailTemplate->body;
-
-		$data = array_merge($data,\Dashboard\DashboardController::get_instance()->getSetupThemes());
-		
-		return \View::make( $data['view_path'] . '.settings.email.update_template', $data );
-	}
-
-	public function postUpdateTemplate($id)
-	{
-		$emailTemplate = \EmailTemplate\EmailTemplate::find($id);
-		$emailTemplate->name 		= \Input::get('template_name', '');
-		$emailTemplate->subject 	= \Input::get('template_subject', '');
-		$emailTemplate->body 		= \Input::get('template_body', '');
-		$emailTemplate->save();
-
-		if($emailTemplate) {
-			\Session::flash('message', 'Successfully Updated Template');
-		} else {
-			\Session::flash('message', 'Something went wrong');
-		}
-
-		return \Redirect::to('settings/email');		
-	}
-
-	public function getRemoveTemplate($id)
-	{
-		$emailTemplate = \EmailTemplate\EmailTemplate::find($id);
-
-		if($emailTemplate->belongs_to == \Auth::id()) {
-			$emailTemplate->delete();
-			\Session::flash('message', 'Successfully Removed Template');
-		} else {
-			\Session::flash('message', 'Template does not belong to the logged in user.');
-		}
-
-		return \Redirect::to('settings/email');		
-	}
-
-	public function postSaveSignature()
-	{
-		$emailSignature = new \EmailSignature\EmailSignature;
-		$emailSignature->belongs_to = \Auth::id();
-		$emailSignature->name 		= \Input::get('signature_name', '');
-		$emailSignature->body 		= \Input::get('signature_body', '');
-		$emailSignature->save();
-
-		if($emailSignature) {
-			\Session::flash('message', 'Successfully Added Signature');
-		} else {
-			\Session::flash('message', 'Something went wrong');
-		}
-
-		return \Redirect::to('settings/email');
-	}
-
-	public function getUpdateSignature($id)
-	{
-		$emailSignature 		= \EmailSignature\EmailSignature::find($id);
-		$data 					= $this->data_view;
-		$data['portlet_title'] 	= 'Update Template';
-		$data['id']			= $emailSignature->id;
-		$data['name'] 		= $emailSignature->name;
-		$data['body']		= $emailSignature->body;
-
-		$data = array_merge($data,\Dashboard\DashboardController::get_instance()->getSetupThemes());
-		
-		return \View::make( $data['view_path'] . '.settings.email.update_signature', $data );
-	}
-
-	public function postUpdateSignature($id)
-	{
-		$emailSignature = \EmailSignature\EmailSignature::find($id);
-		$emailSignature->name 		= \Input::get('signature_name', '');
-		$emailSignature->body 		= \Input::get('signature_body', '');
-		$emailSignature->save();
-
-		if($emailSignature) {
-			\Session::flash('message', 'Successfully Updated Signature');
-		} else {
-			\Session::flash('message', 'Something went wrong');
-		}
-
-		return \Redirect::to('settings/email');	
-	}
-
-	public function getRemoveSignature($id)
-	{
-		$emailSignature = \EmailSignature\EmailSignature::find($id);
-
-		if($emailSignature->belongs_to == \Auth::id()) {
-			$emailSignature->delete();
-			\Session::flash('message', 'Successfully Removed Signature');
-		} else {
-			\Session::flash('message', 'Signature does not belong to the logged in user.');
-		}
-
-		return \Redirect::to('settings/email');		
-	}
-
-	public function getTemplate($id) {
-		$emailTemplate = \EmailTemplate\EmailTemplate::find($id);
-		return \Response::json($emailTemplate);
-	}
-
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function getTemplate($id)
+    {
+        $emailTemplate = \EmailTemplate\EmailTemplate::find($id);
+        return \Response::json($emailTemplate);
+    }
 }
--- app/helpers/EmailShortCodeReplacement.php
+++ app/helpers/EmailShortCodeReplacement.php
@@ -3,74 +3,74 @@
 
 class EmailShortCodeReplacement {
 
-	protected static $instance = null;
+    protected static $instance = null;
 
-	/**
-	 * Return an instance of this class.
-	 *
-	 *
-	 * @return    object    A single instance of this class.
-	 */
-	public static function get_instance() {
+    /**
+     * Return an instance of this class.
+     *
+     *
+     * @return    object    A single instance of this class.
+     */
+    public static function get_instance()
+    {
+        // If the single instance hasn't been set, set it now.
+        if(null == self::$instance) {
+            self::$instance = new self;
+        }
+        return self::$instance;
+    }
 
-		// If the single instance hasn't been set, set it now.
-		if ( null == self::$instance ) {
-			self::$instance = new self;
-		}
-
-		return self::$instance;
-	}
-
-	public function replace($custObj, $body) {
-
-		$data['body'] = $body;
-
-		$data['body'] = str_replace("{ReferenceNo}", $custObj->ref ?:"N/A", $data['body']);
-		$data['body'] = str_replace("{Title}", $custObj->title ?:"N/A", $data['body']);
-		$data['body'] = str_replace("{Firstname}", $custObj->first_name ?:"N/A", $data['body']);
-		$data['body'] = str_replace("{Lastname}", $custObj->last_name ?:"N/A", $data['body']);
-		$data['body'] = str_replace("{Gender}", $custObj->gender ?:"N/A", $data['body']);
-		$data['body'] = str_replace("{Birthdate}", $custObj->dob ? date("d/m/Y",strtotime($custObj->dob)):"N/A", $data['body']);
-		$data['body'] = str_replace("{Marital-Status}", $custObj->marital_status ?:"N/A", $data['body']);
-		$data['body'] = str_replace("{Living-Status}", $custObj->living_status ?:"N/A", $data['body']);
-		$data['body'] = str_replace("{Employment-Status}", $custObj->employment_status ?:"N/A", $data['body']);
-
-		if($custObj->emails()->count() > 0) {
-			$emails = $custObj->emails()->get();
-			foreach($emails as $email) {
-				$data['body'] = str_replace("{Email:".$email->type."}", $email->email ?:"N/A", $data['body']);
-			}
-		}
-		if($custObj->address()->count() > 0) {
-			$addresses = $custObj->address()->get();
-			foreach($addresses as $address) {
-				$data['body'] = str_replace("{Address:".$address->type.":Line1}", $address->address_line_1 ?:"N/A", $data['body']);
-				$data['body'] = str_replace("{Address:".$address->type.":HouseNumber}", $address->address_line_2 ?:"N/A", $data['body']);
-				$data['body'] = str_replace("{Address:".$address->type.":Postcode}", $address->postcode ?:"N/A", $data['body']);
-				$data['body'] = str_replace("{Address:".$address->type.":Town}", $address->town ?:"N/A", $data['body']);
-				$data['body'] = str_replace("{Address:".$address->type.":County}", $address->county ?:"N/A", $data['body']);
-			}
-		}
-		if($custObj->telephone()->count() > 0) {
-			$contactnumbers = $custObj->telephone()->get();
-			foreach($contactnumbers as $contactno) {
-				$data['body'] = str_replace("{ContactNumber:".$contactno->type."}", $contactno->number ?:"N/A", $data['body']);
-			}
-		}
-		if($custObj->url()->count() > 0) {
-			$urls = $custObj->url()->get();
-			foreach($urls as $url) {
-				$data['body'] = str_replace("{".$url->website.":".$url->type."}", $url->url, $data['body']);
-			}
-		}
-
+    /**
+     * @param $custObj
+     * @param $body
+     * @return mixed
+     */
+    public function replace($custObj, $body)
+    {
+        $data['body'] = $body;
+        $data['body'] = str_replace("{ReferenceNo}", $custObj->ref ? : "N/A", $data['body']);
+        $data['body'] = str_replace("{Title}", $custObj->title ? : "N/A", $data['body']);
+        $data['body'] = str_replace("{Firstname}", $custObj->first_name ? : "N/A", $data['body']);
+        $data['body'] = str_replace("{Lastname}", $custObj->last_name ? : "N/A", $data['body']);
+        $data['body'] = str_replace("{Gender}", $custObj->gender ? : "N/A", $data['body']);
+        $data['body'] = str_replace("{Birthdate}", $custObj->dob ? date("d/m/Y", strtotime($custObj->dob)) : "N/A", $data['body']);
+        $data['body'] = str_replace("{Marital-Status}", $custObj->marital_status ? : "N/A", $data['body']);
+        $data['body'] = str_replace("{Living-Status}", $custObj->living_status ? : "N/A", $data['body']);
+        $data['body'] = str_replace("{Employment-Status}", $custObj->employment_status ? : "N/A", $data['body']);
+        if($custObj->emails()->count() > 0) {
+            $emails = $custObj->emails()->get();
+            foreach($emails as $email) {
+                $data['body'] = str_replace("{Email:" . $email->type . "}", $email->email ? : "N/A", $data['body']);
+            }
+        }
+        if($custObj->address()->count() > 0) {
+            $addresses = $custObj->address()->get();
+            foreach($addresses as $address) {
+                $data['body'] = str_replace("{Address:" . $address->type . ":Line1}", $address->address_line_1 ? : "N/A", $data['body']);
+                $data['body'] = str_replace("{Address:" . $address->type . ":HouseNumber}", $address->address_line_2 ? : "N/A", $data['body']);
+                $data['body'] = str_replace("{Address:" . $address->type . ":Postcode}", $address->postcode ? : "N/A", $data['body']);
+                $data['body'] = str_replace("{Address:" . $address->type . ":Town}", $address->town ? : "N/A", $data['body']);
+                $data['body'] = str_replace("{Address:" . $address->type . ":County}", $address->county ? : "N/A", $data['body']);
+            }
+        }
+        if($custObj->telephone()->count() > 0) {
+            $contactnumbers = $custObj->telephone()->get();
+            foreach($contactnumbers as $contactno) {
+                $data['body'] = str_replace("{ContactNumber:" . $contactno->type . "}", $contactno->number ? : "N/A", $data['body']);
+            }
+        }
+        if($custObj->url()->count() > 0) {
+            $urls = $custObj->url()->get();
+            foreach($urls as $url) {
+                $data['body'] = str_replace("{" . $url->website . ":" . $url->type . "}", $url->url, $data['body']);
+            }
+        }
         //custom fields
         $customFieldsData = \CustomFieldData\CustomFieldDataEntity::get_instance()->getFieldsDataByCustomer($custObj->id);
         foreach($customFieldsData as $cFieldData) {
             $customField = \CustomField\CustomField::find($cFieldData->field_id);
-            $data['body'] = str_replace("{CustomField:".$customField->name."}", $cFieldData->value, $data['body']);
+            $data['body'] = str_replace("{CustomField:" . $customField->name . "}", $cFieldData->value, $data['body']);
         }
-		return $data['body'];
-	}
-
+        return $data['body'];
+    }
 }
