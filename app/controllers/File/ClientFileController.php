7630963fde5692e86915273fa82648e2e1d813b2
apysais <allan.paul.casilum@gmail.com>
2014-11-21 19:38:23 +0800

    dropbox

--- app/controllers/File/ClientFileController.php
+++ app/controllers/File/ClientFileController.php
@@ -127,7 +127,7 @@ class ClientFileController extends \BaseController {
 		$dropbox_base_file_name = basename($dropbox_file);
 		$file = strtolower($dropbox_base_file_name);
 		$file = preg_replace('/\s+/', '_', $file);
-		$rep_array('%20','%28','%27','%29');
+		$rep_array = array('%20','%28','%27','%29');
 		$file = str_replace($rep_array, '_', $file);
 		$new_file_name = \Auth::id() . '_' . $file;
 		$destination = $this->fileFolder . '/' . preg_replace('/\s+/', '_', $new_file_name);
