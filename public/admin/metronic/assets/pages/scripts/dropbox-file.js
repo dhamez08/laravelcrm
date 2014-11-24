39ccb8baf15de3e0d6122a8cb519bf2ce7b1a252
apysais <allan.paul.casilum@gmail.com>
2014-11-24 22:28:18 +0800

    dropbox redirect

--- public/admin/metronic/assets/pages/scripts/dropbox-file.js
+++ public/admin/metronic/assets/pages/scripts/dropbox-file.js
@@ -20,7 +20,7 @@ var DropBoxIntegrationFile = function() {
 
 		request.done(function( msg ) {
 		   if (typeof msg.redirect != 'undefined') {
-				//window.location.href = msg.redirect;
+				window.location.href = msg.redirect;
 			}
 		});
 	}
