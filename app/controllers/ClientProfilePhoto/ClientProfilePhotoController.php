82f042600c45a196cd3ea46a45421c6b199a1690
dicklague <dick.lague@binstruments.com>
2014-11-27 17:08:53 +0800

    Add Upload Profile Photo on client page.

--- app/controllers/ClientProfilePhoto/ClientProfilePhotoController.php
+++ app/controllers/ClientProfilePhoto/ClientProfilePhotoController.php
@@ -7,6 +7,29 @@ namespace ClientProfilePhoto;
 use \Carbon\Carbon;
 
 class ClientProfilePhotoController extends \BaseController {
+  protected $fileFolder;
+
+    public function __construct(){
+      parent::__construct();
+      $this->fileFolder 	 	=  public_path() . '/img/profile_images';
+    }
+
+    /**
+    * Return an instance of this class.
+    *
+    *
+    * @return    object    A single instance of this class.
+    */
+    public static function get_instance() {
+
+      // If the single instance hasn't been set, set it now.
+      if ( null == self::$instance ) {
+        self::$instance = new self;
+      }
+
+      return self::$instance;
+    }
+
     public function getChangephoto(){
       $id = \Input::get('id',0);
       $accnt = \Input::get('accnt','');
@@ -16,18 +39,49 @@ class ClientProfilePhotoController extends \BaseController {
       $change->save();
 
       if($id > 0){
-        $photo = \CustomerProfileImages\CustomerProfileImages::find($id)->first();
+        $photo = \CustomerProfileImages\CustomerProfileImages::where('id','=',$id)->first();
         if(count($photo) == 0){
+          $profile_photo_id = 0;
           $profile_image = asset('public/img/profile_images/profile.jpg');
           $avatar = asset('public/img/profile_images/profile.jpg');
         } else {
+          $profile_photo_id = $photo->id;
           $profile_image = $photo->image;
-          $avatar = $photo->image;
+          $avatar = $photo->image_thumbnails;
         }
       } else {
+        $profile_photo_id = 0;
         $profile_image = asset('public/img/profile_images/profile.jpg');
         $avatar = asset('public/img/profile_images/profile.jpg');
       }
-      return \Response::json(array('status'=>'change','profile_image'=>$profile_image,'avatar'=>$avatar));
+      return \Response::json(array('status'=>'change','profile_photo_id'=>$profile_photo_id,'profile_image'=>$profile_image,'avatar'=>$avatar));
     }
+
+    public function postUpload(){
+      $url = \Input::get('url');
+      $customer = \Input::get('customer_id',\Auth::id());
+      $photo = \Input::file('profile-photo-upload-file');
+      $extension = $photo->getClientOriginalExtension();
+      $distination = $this->fileFolder;
+      $filename = str_random(30).'.'.$extension;
+      $ph = array();
+      if ($photo->isValid())
+      {
+        $photo->move($distination, $filename);
+        $save = new \CustomerProfileImages\CustomerProfileImages();
+        $save->customer_id = $customer;
+        $save->image = asset('public/img/profile_images/'.$filename);
+        $save->reference_id = $customer;
+        $save->reference_name = 'site';
+        $save->image_thumbnails = asset('public/img/profile_images/'.$filename);
+        if($save->save()){
+          $change = \Clients\Clients::find($customer);
+          $change->profile_image = $save->id;
+          $change->save();
+        }
+      }
+      \Session::flash('message', 'Profile Photo was successfully uploaded.');
+      return \Redirect::to($url);
+    }
+
 }
--- app/views/themes/admin/metronic/clients/partials/clientProfilePhotoWidget.blade.php
+++ app/views/themes/admin/metronic/clients/partials/clientProfilePhotoWidget.blade.php
@@ -1,3 +1,7 @@
+@section('head-custom-css')
+  @parent
+  <link rel="stylesheet" type="text/css" href="{{$asset_path}}/global/plugins/bootstrap-fileinput/bootstrap-fileinput.css"/>
+@stop
 <div class="modal fade socialProfile ajaxModal" tabindex="-1" role="dialog" aria-labelledby="socialProfile" aria-hidden="true">
   <div class="modal-dialog modal-lg">
     <div class="modal-content">
@@ -7,7 +11,7 @@
       </div>
       <div class="modal-body">
         <div class="row">
-          <div class="col-sm-2 col-md-2 col-lg-2">
+          <div style="width:100px;height:100px;min-width:100px;min-height:100px;max-width:100px;max-height:100px;float:left;">
             <a href="#" class="thumbnail photo-select" data-photo-id="0">
               <img src="{{url('public/img/profile_images/profile.jpg')}}" title="" alt="" />
             </a>
@@ -15,7 +19,7 @@
           @if(isset($customer))
             <?php $photo = \CustomerProfileImages\CustomerProfileImages::find($customer->id)->get(); ?>
             @foreach($photo as $pics)
-            <div class="col-sm-2 col-md-2 col-lg-2">
+            <div style="width:100px;height:100px;min-width:100px;min-height:100px;max-width:100px;max-height:100px;float:left;">
               <a href="#" class="thumbnail photo-select" data-photo-id="{{$pics->id}}">
                 <img src="{{$pics->image}}" title="" alt="" />
               </a>
@@ -26,7 +30,30 @@
         <hr >
         <div class="row">
           <div class="col-md-12">
-            TODO: UPLOAD FROM COMPUTER
+            <form action="{{url('clientprofile/upload')}}" method="POST" enctype="multipart/form-data">
+              {{Form::token()}}
+              <input type="hidden" name="url" value="{{\Request::url()}}" />
+              <div class="fileinput fileinput-new" data-provides="fileinput">
+                <div class="fileinput-new thumbnail" style="width: 100px; height: 100px;">
+                  <img src="http://www.placehold.it/100x100/EFEFEF/AAAAAA&amp;text=no+image" alt=""/>
+                </div>
+                <div class="fileinput-preview fileinput-exists thumbnail" style="max-width: 100px; max-height: 10 0px;">
+                </div>
+                <div>
+                  <span class="btn small default btn-file">
+                    <span class="fileinput-new">Select image</span>
+                    <span class="fileinput-exists">Change</span>
+                    <input type="file" name="profile-photo-upload-file">
+                  </span>
+                  <a href="#" class="small btn red fileinput-exists" data-dismiss="fileinput">Remove</a>
+                  <input type="submit" class="small btn green fileinput-exists" name="save" value="Save Profile Pcture">
+                </div>
+              </div>
+              <div class="clearfix margin-top-10">
+                <span class="label label-danger">NOTE!</span>
+                  Image preview only works in IE10+, FF3.6+, Safari6.0+, Chrome6.0+ and Opera11.1+. In older browsers the filename is shown instead.
+              </div>
+            </form>
           </div>
         </div>
         <div class="clearfix"></div>
@@ -39,6 +66,7 @@
 </div>
 @section('footer-custom-js')
   @parent
+  <script type="text/javascript" src="{{$asset_path}}/global/plugins/bootstrap-fileinput/bootstrap-fileinput.js"></script>
   <script>
   jQuery(function(e){
     jQuery("a.photo-select").on("click",function(e){
