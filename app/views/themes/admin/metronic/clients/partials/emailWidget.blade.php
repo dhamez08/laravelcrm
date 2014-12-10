6a7549e023258b4b4d0eb5e634d6a8656889280f
dicklague <dick.lague@binstruments.com>
2014-12-10 15:15:39 +0800

    Added Email Popup

--- app/views/themes/admin/metronic/clients/partials/emailWidget.blade.php
+++ app/views/themes/admin/metronic/clients/partials/emailWidget.blade.php
@@ -28,8 +28,8 @@
                   <div class="inbox-form-group mail-to">
                     <label class="control-label">To:</label>
                     <div class="controls controls-to">
-                      <select id="select2_user" class="form-control select2" multiple placeholder="Se;ect Customer's Email">
-                        @if( $email->count() > 1 )
+                      <select id="select2_user" class="form-control select2" multiple placeholder="Select Customer's Email">
+                        @if( $email->count() > 0 )
                           @foreach($email->get() as $mail)
                             <option value="{{ $customer->id }}" selected="selected">{{ $mail->email }}</option>
                           @endforeach
@@ -63,8 +63,7 @@
                             null,
                             array(
                             'class'=>'form-control',
-                            'id'=>'email_subject',
-                            'style'=>'width:200px;'
+                            'id'=>'email_subject'
                             )
                             );
                           }}
@@ -162,69 +161,7 @@
                           </div>
                         </div>
                       </div>
-                      <!--
-                      <div class="inbox-compose-attachment">
-                      <span class="btn green fileinput-button">
-                      <i class="fa fa-plus"></i>
-                      <span>
-                      Add local files... </span>
-                      <input type="file" name="files[]" multiple>
-                    </span>
-                    <table role="presentation" class="table table-striped margin-top-10">
-                    <tbody class="files">
-                  </tbody>
-                </table>
-              </div> -->
-              <script id="template-upload" type="text/x-tmpl">
-                {% for (var i=0, file; file=o.files[i]; i++) { %}
-                <tr class="template-upload fade">
-                  <td class="name" width="30%"><span>{%=file.name%}</span></td>
-                  <td class="size" width="40%"><span>{%=o.formatFileSize(file.size)%}</span></td>
-                  {% if (file.error) { %}
-                  <td class="error" width="20%" colspan="2"><span class="label label-danger">Error</span> {%=file.error%}</td>
-                  {% } else if (o.files.valid && !i) { %}
-                  <td>
-                    <p class="size">{%=o.formatFileSize(file.size)%}</p>
-                    <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0">
-                      <div class="progress-bar progress-bar-success" style="width:0%;"></div>
-                    </div>
-                  </td>
-                  {% } else { %}
-                  <td colspan="2"></td>
-                  {% } %}
-                  <td class="cancel" width="10%" align="right">{% if (!i) { %}
-                    <button class="btn btn-sm red cancel">
-                      <i class="fa fa-ban"></i>
-                      <span>Cancel</span>
-                    </button>
-                    {% } %}</td>
-                  </tr>
-                  {% } %}
-                </script>
-                <!-- The template to display files available for download -->
-                <script id="template-download" type="text/x-tmpl">
-                  {% for (var i=0, file; file=o.files[i]; i++) { %}
-                  <tr class="template-download fade">
-                    {% if (file.error) { %}
-                    <td class="name" width="30%"><span>{%=file.name%}</span></td>
-                    <td class="size" width="40%"><span>{%=o.formatFileSize(file.size)%}</span></td>
-                    <td class="error" width="30%" colspan="2"><span class="label label-danger">Error</span> {%=file.error%}</td>
-                    {% } else { %}
-                    <td class="name" width="30%">
-                      <a href="{%=file.url%}" title="{%=file.name%}" data-gallery="{%=file.thumbnail_url&&'gallery'%}" download="{%=file.name%}">{%=file.name%}</a>
-                    </td>
-                    <td class="size" width="40%"><span>{%=o.formatFileSize(file.size)%}</span></td>
-                    <td colspan="2"></td>
-                    {% } %}
-                    <td class="delete" width="10%" align="right">
-                      <button class="btn default btn-sm" data-type="{%=file.delete_type%}" data-url="{%=file.delete_url%}"{% if (file.delete_with_credentials) { %} data-xhr-fields='{"withCredentials":true}'{% } %}>
-                        <i class="fa fa-times"></i>
-                      </button>
-                    </td>
-                  </tr>
-                  {% } %}
-                </script>
-                <div class="inbox-compose-btn">
+                  <div class="inbox-compose-btn">
                   <button type="submit" name="btn_action" value="send" class="btn blue"><i class="fa fa-check"></i>Send</button>
                   <button type="button" onclick="history.back(-1);" class="btn inbox-discard-btn">Cancel</button>
                   <button type="submit" name="btn_action" value="draft" class="btn">Draft</button>
