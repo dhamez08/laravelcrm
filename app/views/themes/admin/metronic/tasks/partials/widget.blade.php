d08f8c1488c91f0b1b0092607e5f6411d73d3d25
Richard Joseph Porter <rjosephporter@gmail.com>
2014-12-29 17:35:11 +0800

    Client Summary - added action label icons for task widget

--- app/views/themes/admin/metronic/tasks/partials/widget.blade.php
+++ app/views/themes/admin/metronic/tasks/partials/widget.blade.php
@@ -22,17 +22,16 @@
 		<p>Overdue : {{$tasks['due']->all}}</p>
 		<div class="scroller" style="height:256px" data-rail-visible="1" data-rail-color="yellow" data-handle-color="#a1b2bd">
 			<!-- START TASK LIST -->
-			<!-- <ul class="task-list"> -->
-			<ul class="feeds">
+			<!--
+			<ul class="feeds hidden">
 				@if(count($tasks['total']) > 0)
 					@foreach($tasks['data'] as $task)
 						<li>
 							<div class="col1" style="width:70% !important">
 								<div class="cont">
 									<div class="cont-col1">
-										<!-- TODO: Replace with dynamic icon -->
 										<div class="label label-sm label-info">
-											<i class="fa fa-file"></i>
+											<i class="fa {{ $task->label->icons }}"></i>
 										</div>
 										{{$task->displayHtmlLabelIcon(false)}}
 									</div>
@@ -59,53 +58,43 @@
 								</div>
 							</div>
 						</li>
-
-
-						<!--
-						<li class="">
-							<div class="task-title">
-								{{--$task->displayHtmlTaskDue()--}}
-								{{\Carbon\Carbon::parse($task->created_at)->toDateString()}}
-								&nbsp;
-								<span class="task-title-sp">
-									<a class="openModal" data-toggle="modal" data-target=".ajaxModal" href="{{action('Task\TaskController@getEditClientTask',array('id'=>$task->id,'customerid'=>$task->customer_id,'redirect'=>'task'))}}">
-										{{$task->displayName()}}
-									</a>
-								</span>
-								{{$task->displayHtmlLabelIcon()}}
-								&nbsp;
-								<span>
-									For
-									<a href="{{action('Clients\ClientsController@getClientSummary',array('id'=>$task->customer_id))}}">
-									{{$task->displayTaskFullName()}}
-									</a>
-								</span>
-								<div class="task-config">
-									<div class="task-config-btn btn-group hide">
-										<a class="btn btn-xs default" href="#" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
-										<i class="fa fa-cog"></i><i class="fa fa-angle-down"></i>
-										</a>
-										<ul class="dropdown-menu pull-right">
-											<li>
-												<a class="complete-task" href="{{action('Task\TaskController@getCompleteTask',array('id'=>$task->id,'customerid'=>$task->customer_id))}}">
-												<i class="fa fa-check"></i> Complete </a>
-											</li>
-											<li>
-												<a class="openModal" data-toggle="modal" data-target=".ajaxModal" href="{{action('Task\TaskController@getEditClientTask',array('id'=>$task->id,'customerid'=>$task->customer_id))}}">
-												<i class="fa fa-pencil"></i> Edit </a>
-											</li>
-											<li>
-												<a class="delete-task" href="{{action('Task\TaskController@getCancelTask',array('id'=>$task->id,'customerid'=>$task->customer_id))}}">
-												<i class="fa fa-trash-o"></i> Cancel </a>
-											</li>
-										</ul>
-									</div>
-								</div>
-						</li>
-						-->
 					@endforeach
 				@endif
 			</ul>
+			-->
+			<table class="table table-condensed table-feeds">
+				<tbody>
+				@if(count($tasks['total']) > 0)
+					@foreach($tasks['data'] as $task)
+						<tr>
+							<td class="text-center">
+								<div class="label label-sm label-info label-icon">
+									<i class="fa {{ $task->label->icons }}"></i>
+								</div>
+							</td>
+							<td>
+								{{$task->displayHtmlLabelIcon(false)}}
+							</td>
+							<td>
+								<a class="openModal" data-toggle="modal" data-target=".ajaxModal" href="{{action('Task\TaskController@getEditClientTask',array('id'=>$task->id,'customerid'=>$task->customer_id,'redirect'=>'task'))}}">
+									{{$task->displayName()}}
+								</a>								
+							</td>
+							<td>
+								<small class="muted">on {{\Carbon\Carbon::parse($task->created_at)->format('d/m/Y')}} at {{\Carbon\Carbon::parse($task->created_at)->format('H:i')}}</small>
+							</td>
+							<td>
+								<a href="{{
+				 						action('Task\TaskController@getCancelTask',
+				 							array('id'=>$task->id,'customerid'=>$task->customer_id))
+			 							}}" class="pull-right delete-task" title="Delete Task"><i class="icon-trash"></i> 
+			 					</a>								
+							</td>
+						</tr>
+					@endforeach
+				@endif					
+				</tbody>
+			</table>
 		</div>
 		<p>Today : {{$tasks['due']->today}}</p>
 		<p>Next Seven Days : {{$tasks['due']->seven}}</p>
