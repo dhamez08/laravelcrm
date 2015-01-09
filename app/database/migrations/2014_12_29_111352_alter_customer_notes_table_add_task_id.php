0bf8059e5c68180fca656102b6d3d94e793e5cc1
Tristan Flor <tristan.flor@thetmsway.com>
2015-01-10 01:59:11 +0800

    Fix migration problem having two migration with the same class name

--- app/database/migrations/2014_12_29_111352_alter_customer_notes_table_add_task_id.php
+++ app/database/migrations/2014_12_29_111352_alter_customer_notes_table_add_task_id.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterCustomerNotesTable extends Migration {
+class AlterCustomerNotesTableAddTaskId extends Migration {
 
 	/**
 	 * Run the migrations.
