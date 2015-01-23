05f345d70f0934392a97107464540b34953fc71a
Tristan Flor <tristan.flor@thetmsway.com>
2015-01-23 18:29:07 +0800

    Add text if thumbnail has no preview

--- public/admin/metronic/assets/pages/scripts/file-preview.js
+++ public/admin/metronic/assets/pages/scripts/file-preview.js
@@ -6,11 +6,18 @@ $(function(){
         var image = $(this);
         var thumb = image.data('thumb');
 
+
         var options = new Object();
         options.html = true;
-        options.content = $('<img>').attr('src',thumb).css('width', 240);
         options.placement = 'bottom';
         options.container = 'body';
+
+        if(thumb){
+            options.content = $('<img>').attr('src',thumb).css('width', 240);
+        } else {
+            options.content = $('<span>').text('No Preview Available');
+        }
+
 
         $(this).popover(options);
         $(this).popover('show');
