b52e0f15a88701a64ad3b93548cdf7376bbdbaa6
Richard Joseph Porter <rjosephporter@gmail.com>
2015-02-27 15:40:53 +0800

    quick fix

--- app/controllers/Api/ApiController.php
+++ app/controllers/Api/ApiController.php
@@ -1,7 +1,7 @@
 <?php
 namespace Api;
 use Carbon\Carbon;
-class ApiController extends Controller{
+class ApiController extends \Controller{
 	
 	public function postVmdShared() {
 		$new_file = array(
