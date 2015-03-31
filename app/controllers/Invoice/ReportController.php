fe4165922289330c6d0e937f671190ca51d4f88c
Richard Joseph Porter <rjosephporter@gmail.com>
2015-03-31 15:17:08 +0800

    invoice reports

--- app/controllers/Invoice/ReportController.php
+++ app/controllers/Invoice/ReportController.php
@@ -6,9 +6,56 @@ use View;
 
 class ReportController extends \BaseController {
 
-	protected $layout = 'index';
-	
-	
+	/**
+	 * Instance of this class.
+	 *
+	 * @var      object
+	 */
+	protected static $instance = null;
+
+	/**
+	 * hold the view essentials like
+	 * - title
+	 * - view path
+	 * @return array | associative
+	 * */
+	protected $data_view;
+
+	/**
+	 * auto setup initialize object
+	 * */
+	public function __construct(){
+		parent::__construct();
+		$this->data_view = parent::setupThemes();
+		$this->data_view['dashboard_index'] = $this->data_view['view_path'] . '.dashboard.index';
+	}
+
+	/**
+	 * Return an instance of this class.
+	 *
+	 *
+	 * @return    object    A single instance of this class.
+	 */
+	public static function get_instance() {
+
+		// If the single instance hasn't been set, set it now.
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * get themes
+	 * @return	array
+	 * */
+	public function getSetupThemes(){
+		$this->data_view['html_body_class'] = 'page-header-fixed page-quick-sidebar-over-content page-sidebar-closed-hide-logo page-container-bg-solid page-full-width';
+		$this->data_view['header_class'] = 'page-header navbar navbar-fixed-top';
+		return $this->data_view;
+	}
+		
 	/* === VIEW === */
 	public function index()
 	{
@@ -19,8 +66,10 @@ class ReportController extends \BaseController {
 			'amounts'	=> $reports->amounts(),
 			'clients'	=> $reports->clients()
 		);
+
+		$data += $this->getSetupThemes();
 		
-		$this->layout->content = View::make('reports.index', $data);
+		return View::make($data['view_path'] . '.invoice.reports.index', $data);
 	}
 	/* === END VIEW === */
 
