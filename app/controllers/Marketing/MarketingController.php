03749b20d9bbd1d5ce9bdfcf8e929e980d858742
Tristan Flor <tristan.flor@thetmsway.com>
2015-04-14 23:45:36 +0800

    Add css for responsiveness on body

--- app/controllers/Marketing/MarketingController.php
+++ app/controllers/Marketing/MarketingController.php
@@ -663,8 +663,9 @@ class MarketingController extends \BaseController {
                 $template = \UserEmailTemplate\UserEmailTemplate::find($template_id);
                 $body = \HTML::decode($template['source_code']);
 
+                $data['body'] = "<style> @media (max-width: 640px) { .section-area { width: 100% !important; padding: 0 !important; } .inner-section { padding: 10px 0 !important; } .inner-section div { width: 100% !important; display: block !important; margin: 0 auto !important; text-align: center !important; padding: 5px 0 !important; } .scaled-image { width: 80% !important; height: auto !important; } p { text-align: center !important; } .inner-section div.div-button { width: 90% !important; margin-top: 10px !important; margin-bottom: 10px !important; } } </style>";
                 $body = str_replace('contenteditable="true"','',$body);
-                $data['body'] = str_replace('contenteditable','',$body);
+                $data['body'] .= str_replace('contenteditable','',$body);
             }
 
             $emails = \Input::get('email');
