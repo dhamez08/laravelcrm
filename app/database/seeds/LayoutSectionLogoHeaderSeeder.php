8606b531c69ccbabd9cc25ed045e53d517096eac
Tristan Flor <tristan.flor@thetmsway.com>
2015-05-14 01:36:36 +0800

    Enable reseeding parent row

--- app/database/seeds/LayoutSectionLogoHeaderSeeder.php
+++ app/database/seeds/LayoutSectionLogoHeaderSeeder.php
@@ -10,12 +10,12 @@ class LayoutSectionLogoHeaderSeeder extends Seeder {
     public function run()
     {
         Eloquent::unguard();
-//        DB::table('system_email_layout')->insert(array(
-//            array(
-//                'name'=>'Logo Headers',
-//                'is_generic' => true
-//            )
-//        ));
+        DB::table('system_email_layout')->insert(array(
+            array(
+                'name'=>'Logo Headers',
+                'is_generic' => true
+            )
+        ));
         DB::table('system_layout_section')->insert(array(
             array(
                 'layout_id' => 12,
