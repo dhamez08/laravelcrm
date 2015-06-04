1a7aeb39955a531c5104e3680a82a06b805f7457
prime <prime@prime-pc>
2015-06-04 22:37:07 +0800

    add field to messages table

---
+++ app/database/migrations/2015_06_04_142429_alter_messages_table_add_receipt.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterMessagesTableAddReceipt extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		 Schema::table('messages', function($table)
+        {
+            $table->tinyInteger('receipt')->after('deleted_at');
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		 Schema::table('messages', function($table)
+        {
+            $table->dropColumn('receipt');
+        });
+	}
+
+}
