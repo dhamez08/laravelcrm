b3757b92b8b2d0380c00e70412e0f8c08ea0a353
prime <prime@prime-pc>
2015-06-22 20:47:00 +0800

    fix home link

--- app/views/themes/admin/metronic/dashboard/partials/logo.blade.php
+++ app/views/themes/admin/metronic/dashboard/partials/logo.blade.php
@@ -1,6 +1,6 @@
 <!-- BEGIN LOGO -->
 <div class="page-logo">
-	<a href="index.html">
+	<a href="/">
 	<img class="logo-default" alt="logo" src="{{$asset_path}}/layout/img/logo_123crm.png">
 	</a>
 	<div class="menu-toggler sidebar-toggler hide">
