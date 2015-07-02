615f3e72754feebfff0f892884a1c677f8dca144
Tristan Flor <tristan.flor@thetmsway.com>
2015-07-02 22:40:10 +0800

    Filter to only 3 notification, filter also those that has not been reminded yet

--- app/views/themes/admin/metronic/tasks/partials/taskNotification.blade.php
+++ app/views/themes/admin/metronic/tasks/partials/taskNotification.blade.php
@@ -1,18 +1,21 @@
 @if(count($tasks) > 0)
-    @foreach($tasks as $task)
-        <li>
-            <a class="openModal" data-toggle="modal" data-target=".ajaxModal" href="{{action('Task\TaskController@getEditClientTask',array('id'=>$task->id,'customerid'=>$task->customer_id,'redirect'=>'task'))}}">
-                <span class="task">
-                    <span class="desc">
-                        {{$task->displayHtmlTaskDue()}}
-                        {{$task->displayHtmlLabelIcon()}}
-                        <br />
-                        {{$task->displayName()}}
-                        <br />
-                        {{$task->displayTaskFullName()}}
+    @for($counter = 0; $counter < 3; $counter++)
+        {{--*/ $task = $tasks[$counter] /*--}}
+        @if(!$task->isReminded())
+            <li>
+                <a class="openModal" data-toggle="modal" data-target=".ajaxModal" href="{{action('Task\TaskController@getEditClientTask',array('id'=>$task->id,'customerid'=>$task->customer_id,'redirect'=>'task'))}}">
+                    <span class="task">
+                        <span class="desc">
+                            {{$task->displayHtmlTaskDue()}}
+                            {{$task->displayHtmlLabelIcon()}}
+                            <br />
+                            {{$task->displayName()}}
+                            <br />
+                            {{$task->displayTaskFullName()}}
+                        </span>
                     </span>
-                </span>
-            </a>
-        </li>
-    @endforeach
+                </a>
+            </li>
+        @endif
+    @endfor
 @endif
