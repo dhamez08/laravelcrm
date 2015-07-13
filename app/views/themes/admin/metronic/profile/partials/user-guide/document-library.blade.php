b47b9b8a8da0333485e7eb3c4049e3d0802c0fde
Tristan Flor <tristan.flor@thetmsway.com>
2015-07-14 01:17:13 +0800

    Add user guide for uploading and deleting files in document library

---
+++ app/views/themes/admin/metronic/profile/partials/user-guide/document-library.blade.php
@@ -0,0 +1,125 @@
+<div id="document-library" class="tab-pane">
+    <div id="document-library-group" class="panel-group">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#document-library-group" href="#document-library-0">
+                        Document Library </a>
+                </h4>
+            </div>
+            <div id="document-library-0" class="panel-collapse collapse in">
+                <div class="panel-body">
+                    <p>
+                        The Document Library allows you to organize the files you use to manage your clients. You can upload documents here which you can later on use on your business activities such as attaching the documents in your emails. You can also organize your documents by filing them into sections and subsections for easier access.
+                    </p>
+                    <p>
+                        You can access the <strong>Document Library</strong> page in the top menu.
+                    </p>
+                    <img class="thumbnail" src="{{url('/public/img/help_and_settings/document_library/top_bar.png')}}" /><p>
+                        Upon entering the page, you will be presented with an area where you can create sections and upload files to those sections.
+                    </p>
+                    <img class="thumbnail" src="{{url('/public/img/help_and_settings/document_library/document_library.png')}}" /><p>
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#document-library-group" href="#document-library-1">
+                        Sections and Subsections </a>
+                </h4>
+            </div>
+            <div id="document-library-1" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <p>
+                        This crm provides you with an Email Marketing tool. This allows you to send marketing emails to multiple clients at once. You can create templates which you can reuse and send again to other clients in the future. You can either use plain text emails or use our predefined email layouts which you can modify to your preference to make your email more appealing to your clients.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#document-library-group" href="#document-library-2">
+                        Manipulating Sections </a>
+                </h4>
+            </div>
+            <div id="document-library-2" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <p>
+                        This crm provides you with an Email Marketing tool. This allows you to send marketing emails to multiple clients at once. You can create templates which you can reuse and send again to other clients in the future. You can either use plain text emails or use our predefined email layouts which you can modify to your preference to make your email more appealing to your clients.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#document-library-group" href="#document-library-3">
+                        Uploading Documents </a>
+                </h4>
+            </div>
+            <div id="document-library-3" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <p>
+                        After creating section and subsections, you can start uploading your documents. You can organize your documents by clicking the  <i class="fa fa-upload"></i>  on the specific section where you want the file to be uploaded.
+                    </p>
+                    <img class="thumbnail" src="{{url('/public/img/help_and_settings/document_library/upload_file.png')}}" />
+                    <p>
+                        After clicking the upload button, you will be shown a file uploader.
+                    </p>
+                    <img class="thumbnail" src="{{url('/public/img/help_and_settings/document_library/uploader.png')}}" />
+                    <p>
+                        There are two ways in which you can upload your documents:
+                    </p>
+                    <ol style="color: #636363">
+                        <li>Clicking the <strong><i class="fa fa-plus"></i> Add files...</strong> button and browsing through your files.</li>
+                        <li>Dragging files to the drop area.</li>
+                    </ol>
+                    <p>
+                        These file uploader support multiple file uploads so you can highlight several files or drag multiple files for simultaneous upload.
+                    </p>
+                    <p>
+                        After selecting your files, you will be able to view them via the preview. At this state, you can still add additional files. You can also remove files from the list by clicking the <strong><i class="fa fa-ban"></i> Cancel</strong> button.
+                    </p>
+                    <img class="thumbnail" src="{{url('/public/img/help_and_settings/document_library/multi_selected.png')}}" />
+                    <p>
+                        If you are satisfied with your file selection, you can start uploading simultaneously by clicking the <strong><i class="fa fa-upload"></i> Start Upload</strong> or you can upload them individually by clicking <strong><i class="fa fa-upload"></i> Start</strong> button next the file thumbnail.
+                    </p>
+                    <p>
+                        You can also cancel the upload proces by clicking the <strong>Cancel Upload</strong>.
+                    </p>
+                    <img class="thumbnail" src="{{url('/public/img/help_and_settings/document_library/uploading.png')}}" />
+                    <p>
+                        After the upload process has finished, you will then be able to see the files inside the subsection where you uploaded it. You can also view a preview by hovering your mouse to the file name.
+                    </p>
+                    <img class="thumbnail" src="{{url('/public/img/help_and_settings/document_library/file_preview.png')}}" />
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#document-library-group" href="#document-library-4">
+                        Deleting Documents </a>
+                </h4>
+            </div>
+            <div id="document-library-4" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <p>
+                        If you no longer need documents in your library, you can always delete them. There are two ways you can delete the files.
+                    </p>
+                    <ol style="color: #636363">
+                        <li>Click the <strong><i class="fa fa-times"></i> Remove</strong> button beside the file you want to delete</li>
+                        <li>Selecting multiple files using the checkbox and clicking <strong><i class="fa fa-times"></i> Delete Selected</strong> button to bulk delete the selected files</li>
+                    </ol>
+                    <img class="thumbnail" src="{{url('/public/img/help_and_settings/document_library/delete.png')}}" />
+                    <p>
+                        One advantage of using the bulk delete is you can delete files across sections at once.
+                    </p>
+                    <img class="thumbnail" src="{{url('/public/img/help_and_settings/document_library/multi_delete.png')}}" />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
